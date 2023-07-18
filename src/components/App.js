9cd40d2b5e33c74512e7b0f02b7ca626c3307fe4
sajid <mohdsajid8010@gmail.com>
2023-07-18 16:43:07 +0530

    commit1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [formData, setFormData] = useState({ name: "", email: "", password: "" });
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(formData)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" onChange={(e) => setFormData({ ...formData, name: e.target.value })} /><br/>
+
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" onChange={(e) => setFormData({ ...formData, email: e.target.value })} /><br/>
+
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" onChange={(e) => setFormData({ ...formData, password: e.target.value })} /><br/>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
